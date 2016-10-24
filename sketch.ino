8f0d684d20f1fe1af69644baa704cf3cfdc49e2f
JulOk <julok2016@gmail.com>
2016-10-24 23:48:17 +0300

    Добавлены изменения: красный и зеленый цвет для пешехода

--- sketch.ino
+++ sketch.ino
@@ -1,15 +1,22 @@
 const int ledGreen = 8;
 const int ledYellow = 9;
 const int ledRed = 10;
+const int ledPeopleRed = 11;
+const int ledPeopleGreen = 12;
 
 void setup() {
   pinMode(ledGreen, OUTPUT);
   pinMode(ledYellow, OUTPUT);
   pinMode(ledRed, OUTPUT);
+  pinMode(ledPeopleRed, OUTPUT);
+  pinMode(ledPeopleGreen, OUTPUT);
 }
 
 void loop() {
-  // зеленый цвет
+  // красный цвет для пешехода
+  digitalWrite(ledPeopleRed, HIGH);
+  
+  // зеленый цвет для автомобиля
   digitalWrite(ledGreen, HIGH);
   delay(3000);
   digitalWrite(ledGreen, LOW);
@@ -23,13 +30,31 @@ void loop() {
     } 
   }
   
-  // желтый цвет
+  // желтый цвет для автомобиля
   digitalWrite(ledYellow, HIGH);
   delay(1000);
   digitalWrite(ledYellow, LOW);
-
-  // красный цвет
+  
+  // красный цвет для пешехода
+  digitalWrite(ledPeopleRed, LOW);
+  
+  // красный цвет для автомобиля
   digitalWrite(ledRed, HIGH);
+  
+  // зеленый цвет для пешехода
+  digitalWrite(ledPeopleGreen, HIGH);
   delay(2000);
+  digitalWrite(ledPeopleGreen, LOW);
+  delay(400);
+  for (int i = 0; i < 2; i++){
+    digitalWrite(ledPeopleGreen, HIGH);
+    delay(400);
+    digitalWrite(ledPeopleGreen, LOW);
+    if (i<2){
+      delay(400);
+    }
+  }
+
+  // красный цвет для автомобиля
   digitalWrite(ledRed, LOW);
-}
+ }
